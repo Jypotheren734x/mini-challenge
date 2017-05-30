4255ab4daefae649fca5a4fc7848128027021e34
Nickolas Komarnitsky <komarnickolas@gmail.com>
2017-05-30 17:52:34 -0600

    Updated js, added in more functionality

--- js/main.js
+++ js/main.js
@@ -22,27 +22,38 @@ var cap = prompt("Enter a number: ");
 if(cap < 100){
     alert("Your number is less than 100");
 }else{
-    alert("Your nummber was: " + cap + ", which is greater than 100");
+    alert("Your number was: " + cap + ", which is greater than 100");
+}
+
+//Question 4
+var cap = prompt("Enter a number: ");
+cap -= 50;
+if(cap < 100){
+    alert("Your number is less than 100");
+}else{
+    alert("Your number was: " + cap + ", which is greater than 100");
 }
 
 // Question 5
-var string = "String";
-var prompted = prompt("String: ");
+var string = "mitochondria";
+var prompted = prompt("What is the powerhouse of the cell?: ");
 if(string === prompted){
-    alert(string + " is equal to " + prompted);
+    alert("Correct! Mitochondria is the powerhouse of the cell");
 }else{
-    alert(string + " is not equal to " + prompted);
+    alert("Wrong! "+prompted+" is not the powerhouse of the cell");
 }
 
 // Question 6
 function echo(name) {
     console.log(name);
 }
+echo("ECHO, ECHO, echo, echo...");
 
 // Question 7
 function print() {
-    console.log(string + " " + prompted)
+    console.log("Mitochondria is the powerhouse of the cell");
 }
+print();
 
 // Question 8
 function open_door(door) {
@@ -57,7 +68,11 @@ function open_door(door) {
             alert("YOU WON A FREE VACATION TO HAWAII!");
             break;
         default:
-            alert("That isn't a valid door number, pick a number 1-3")
+            alert(door +" isn't a valid door number, pick a number 1-3")
             break;
     }
 }
+open_door(1);
+open_door(2);
+open_door(3);
+open_door(4);
